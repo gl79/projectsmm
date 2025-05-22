981e2a5195a249ef751d36b808fd6c8f7ad8e0c9
Gery Liam <77185808+gl79@users.noreply.github.com>
2025-05-22 20:20:20 +0700

    Update index.php

--- public/frontend/index.php
+++ public/frontend/index.php
@@ -443,18 +443,18 @@
   <div id="preloader"></div>
 
   <!-- Vendor JS Files -->
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
-  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-  <script src="assets/vendor/aos/aos.js"></script>
-  <script src="assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
-  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="assets/vendor/drift-zoom/Drift.min.js"></script>
-  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="/frontend/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="/frontend/assets/vendor/php-email-form/validate.js"></script>
+  <script src="/frontend/assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="/frontend/assets/vendor/aos/aos.js"></script>
+  <script src="/frontend/assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
+  <script src="/frontend/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="/frontend/assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="/frontend/assets/vendor/drift-zoom/Drift.min.js"></script>
+  <script src="/frontend/assets/vendor/purecounter/purecounter_vanilla.js"></script>
 
   <!-- Main JS File -->
-  <script src="assets/js/main.js"></script>
+  <script src="/frontend/assets/js/main.js"></script>
 
 </body>
 
